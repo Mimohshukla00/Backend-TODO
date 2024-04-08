33beb6d31afa16c8ca8ba074a9127fa6535bfe16
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-08 23:14:28 +0530

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+  console.log("mimohshukla's sever started");
+});
+
+// add this in package.json
+
+// "start": "node index.js",
+//     "dev":"nodemon index.js"
