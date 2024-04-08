fecd50d70fad8a514e086b0be424462e4d4757a6
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-09 00:39:05 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 // const db = process.env.DATABASE_URL;
 const dbConnect = () => {
-    console.log("mimohshukla")
   // TO FEED the  database with some data, use this code:
-  mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(()=>console.log("Db connection is successfull")).catch((err)=> console.log("error in DB connection"));
-  console.error(error.message)
-  process.exit(1)
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Db connection is successfull"))
+    .catch((err) => console.log("error in DB connection"));
+  console.error(error.message);
+  process.exit(1);
 };
-module.exports=dbConnect;
+module.exports = dbConnect;
