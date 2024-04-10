7d83f3faae0c1d457c6b10a5b24d525df26a1c15
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-10 23:07:56 +0530

    commmit

---
+++ mimoh.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+function middleware(req, res, next) {
+  if (!username === "mimohshukla" || !password === "pass") {
+    req.status(400).json({ message: "Invalid Credentials" });
+  } else {
+    next(); // pass control to the next middleware function
+  }
+}
+
+function kidneyMiddleware(req, res, next) {
+  if (kidneyID != 1 && kidneyID != 2) {
+    req.status(400).json({ message: "Kidney ID is invalid." });
+  } else {
+    next();
+  }
+}
+// define a simple route handler for the home page
+app.get("/healthcheckup", middleware,kidneyMiddleware,function(req, res) {
+    // do something abput your kidney  
+    res.send("your health is healthy");
+} );
