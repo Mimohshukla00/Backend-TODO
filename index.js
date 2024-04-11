57e59699213ce2b2834fc2ce12decad4acf848a3
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-11 13:30:16 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 // "start": "node index.js",
 // "dev":"nodemon index.js"
 
+
+
+// npm install dotenv
+// npm install express
+// npm install mongoose
+// npm install nodemon
+
 const express = require("express");
 const app = express();
 
@@ -13,4 +20,18 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 // import routes for todo api
 
-const todoRoutes = require("./routes/todoRoutes");
+const todoRoutes = require("./routes/todos");
+// mount the todo Api routes
+
+app.use("/api/v1", todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT} `);
+});
+
+
+// connect to the database 
+
+const dbconnect = require("./config/database")
+dbconnect();
+
