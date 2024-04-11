820986986c590a03011d6384357680f79e46d080
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-11 12:41:35 +0530

    commit todo

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -1,7 +1,27 @@
 // create controller of todo list
-const Todo =require("..models/Todo")
+const Todo = require("..models/Todo");
 
-// define route handler 
+// define route handler
 
-
-exports.createTodo=async(req,res)=>{
+exports.createTodo = async (req, res) => {
+  try {
+    // extract title and description from request body
+    const { title, description } = req.body;
+    // create a new todo object and insert in db
+    const response = await Todo.create({ title, description });
+    // send a json response
+    res.status(200).json({
+      status: "success",
+      data: response,
+      message: "Todo created successfully",
+    });
+  } catch (error) {
+    console.error(err);
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      data: "internal server error",
+      message: "Server error",
+    });
+  }
+};
